1e0d8bf5ae71ac451958d8bae4e81cdd7747ee54
Akash Sinha <95168800+Ace-42@users.noreply.github.com>
2022-07-27 23:46:16 +0530

    Bubble sort

--- sort.c
+++ sort.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 int main() {
-  char op;
-  double first_no, second_no;
-  printf("Enter an operation to perform (+, -, *, /): ");
-  scanf("%c", &op);
-  printf("Enter two numbers: ");
-  scanf("%lf %lf", &first_no, &second_no);
-  if(op=='+'){
-    printf("The result of the addition of the given two numbers is:\n");
-    printf("%.2lf + %.2lf = %.2lf",first_no,second_no,first_no+second_no);
+  int i,j,n;
+  printf("Enter how many numbers are for input: ");
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++){
+      printf("Enter element %d: ",i+1);
+      scanf("%d",&arr[i]);
   }
-  else if(op=='-'){
-    printf("The result of the subtraction of the given two numbers is:\n");
-    printf("%.2lf - %.2lf = %.2lf",first_no,second_no,first_no-second_no);
+  void insertionsort(int array[], int size){
+   int temp;
+   for(i=1;i<n;i++){
+         temp=arr[i];
+         j=i-1;
+         while((temp<arr[j])&&(j>=0)){
+            arr[j+1]=arr[j];
+            j=j-1;
+         }
+         arr[j+1]=temp;
+      }
+      printf("The elements in sorted order: ");
+      for(i=0;i<n;i++){
+         printf("%d ",arr[i]);
+      }
   }
-  else if(op=='*'){
-    printf("The result of the multiplication of the given two numbers is:\n");
-    printf("%.2lf * %.2lf = %.2lf",first_no,second_no,first_no*second_no);
+  void bubblesort(int array[],int size){
+      for (int i=0;i<size-1;i++) {
+      for(int j=0;i<size-j-1;j++){
+         if (array[j]>array[j+1]){
+         int temp=array[j];
+         array[j]=array[j+1];
+         array[j+1]=temp;
+         }
+      }
+   }
+   printf("The elements in sorted order: ");
+      for(i=0;i<n;i++){
+         printf("%d ",arr[i]);
+      }
   }
-  else if(op=='/'){
-    printf("The result of the division of the given two numbers is:\n");
-    printf("%.2lf / %.2lf = %.2lf",first_no,second_no,first_no/second_no);
-  }
+  insertionsort(arr,n);
+  bubblesort(arr,n);
   return 0;
 }
