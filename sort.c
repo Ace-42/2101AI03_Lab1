e5629f82b062cf13be9940359c1c3bc0b7e35be1
Akash Sinha <95168800+Ace-42@users.noreply.github.com>
2022-07-27 23:34:37 +0530

    Insertion sort

---
+++ sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+  char op;
+  double first_no, second_no;
+  printf("Enter an operation to perform (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two numbers: ");
+  scanf("%lf %lf", &first_no, &second_no);
+  if(op=='+'){
+    printf("The result of the addition of the given two numbers is:\n");
+    printf("%.2lf + %.2lf = %.2lf",first_no,second_no,first_no+second_no);
+  }
+  else if(op=='-'){
+    printf("The result of the subtraction of the given two numbers is:\n");
+    printf("%.2lf - %.2lf = %.2lf",first_no,second_no,first_no-second_no);
+  }
+  else if(op=='*'){
+    printf("The result of the multiplication of the given two numbers is:\n");
+    printf("%.2lf * %.2lf = %.2lf",first_no,second_no,first_no*second_no);
+  }
+  else if(op=='/'){
+    printf("The result of the division of the given two numbers is:\n");
+    printf("%.2lf / %.2lf = %.2lf",first_no,second_no,first_no/second_no);
+  }
+  return 0;
+}
