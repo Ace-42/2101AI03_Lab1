da4ee5675ca3dffdb1fde547c50daecbab7db020
Akash Sinha <95168800+Ace-42@users.noreply.github.com>
2022-07-28 00:06:41 +0530

    Merge sort

--- sort.c
+++ sort.c
@@ -59,8 +59,54 @@ int main() {
             printf("%d ",arr[i]);
          }
       }
+   void merge(int a[], int start, int mid, int end){
+    int i, j, k;
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = a[start + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = a[mid + 1 + j];
+    i = 0;
+    j = 0;
+    k = start;
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j])
+        {
+         a[k++] = L[i++];
+        }
+        else
+        {
+         a[k++] = R[j++];
+        }
+    }
+    while(i < n1){
+        a[k++] = L[i++];
+    }
+    while (j < n2)
+        start
+        {
+            a[k++] = R[j++];
+        }
+}
+void mergesort(int a[], int start, int end)
+{
+    if (start < end)
+    {
+        int mid = (start + end) / 2;
+        mergesort(a, start, mid);
+        mergesort(a, mid + 1, end);
+        merge(a, start, mid, end);
+    }
+}
   insertionsort(arr,n);
   bubblesort(arr,n);
   selectionsort(arr,n);
+  mergesort(arr,0,n-1);
+  printf("The elements in sorted order: ");
+   for(int i=0;i<n;i++){
+      printf("%d ",arr[i]);
+   }
   return 0;
 }
